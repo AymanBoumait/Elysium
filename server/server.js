996f1a2d2b63c106864df186eb0eed182fd7dba9
Ayman <aymanboumait123@gmail.com>
2023-02-08 18:59:19 +0100

    update 7

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 
 const configuration = new Configuration({
-    apiKey: process.env.OpenAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
